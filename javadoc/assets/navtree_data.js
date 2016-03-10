4e53b99d40ae5405a71ce8f5bf3bdcd2588018d9
Hello <hello@world.com>
2016-03-10 19:29:10 +0530

    Create docs

---
+++ javadoc/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "com.freshdesk.hotline", "com/freshdesk/hotline/package-summary.html", [ [ "Interfaces", null, [ [ "UnreadCountCallback", "com/freshdesk/hotline/UnreadCountCallback.html", null, "1.0.0" ] ]
+, "" ], [ "Classes", null, [ [ "Hotline", "com/freshdesk/hotline/Hotline.html", null, "1.0.0" ], [ "HotlineConfig", "com/freshdesk/hotline/HotlineConfig.html", null, "1.0.0" ], [ "HotlineNotificationConfig", "com/freshdesk/hotline/HotlineNotificationConfig.html", null, "1.0.0" ], [ "HotlineUser", "com/freshdesk/hotline/HotlineUser.html", null, "1.0.0" ] ]
+, "" ], [ "Enums", null, [ [ "HotlineCallbackStatus", "com/freshdesk/hotline/HotlineCallbackStatus.html", null, "1.0.0" ] ]
+, "" ] ]
+, "1.0.0" ] ]
+
+;
+
---
+++ javadoc/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"com.freshdesk.hotline", link:"com/freshdesk/hotline/package-summary.html", type:"package" },
+      { id:1, label:"com.freshdesk.hotline.Hotline", link:"com/freshdesk/hotline/Hotline.html", type:"class" },
+      { id:2, label:"com.freshdesk.hotline.HotlineCallbackStatus", link:"com/freshdesk/hotline/HotlineCallbackStatus.html", type:"class" },
+      { id:3, label:"com.freshdesk.hotline.HotlineConfig", link:"com/freshdesk/hotline/HotlineConfig.html", type:"class" },
+      { id:4, label:"com.freshdesk.hotline.HotlineNotificationConfig", link:"com/freshdesk/hotline/HotlineNotificationConfig.html", type:"class" },
+      { id:5, label:"com.freshdesk.hotline.HotlineUser", link:"com/freshdesk/hotline/HotlineUser.html", type:"class" },
+      { id:6, label:"com.freshdesk.hotline.UnreadCountCallback", link:"com/freshdesk/hotline/UnreadCountCallback.html", type:"class" }
+
+    ];
